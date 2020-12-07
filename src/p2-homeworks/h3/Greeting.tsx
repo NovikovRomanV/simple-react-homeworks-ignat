35c01d35e5ee62901f5db6f7837d30ac0787cf17
NovikovRomanV <rimm19845@gmail.com>
2020-12-07 14:05:23 +0300

    task 3 completed

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,26 +1,30 @@
-import React from "react";
+import React, {ChangeEvent} from "react";
 import s from "./Greeting.module.css";
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+    name: string
+    setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void
+    addUser: () => void
+    error: string
+    totalUsers: number
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error; // need to fix with (?:)
+    const inputClass = error !== '' ? s.error : ''
 
     return (
-        <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
+        <div className={s.container}>
+            <div className={s.boxInputButton}>
+                <input value={name} onChange={setNameCallback} className={inputClass} placeholder={"Name user"}/>
+                <button onClick={addUser}>ADD</button>
+            </div>
+            <span className={s.spanError}>{error}</span>
+            <div className={s.userCounter}>
+                <span>Total users: {totalUsers}</span>
+            </div>
         </div>
     );
 }
